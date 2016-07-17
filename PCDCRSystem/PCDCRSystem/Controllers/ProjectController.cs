958247b101899190acfda45c2e1c07a7e5c41ba6
ainma007 <b-sword@outlook.com>
2016-07-17 12:42:08 +0300

    Add control

---
+++ PCDCRSystem/PCDCRSystem/Controllers/ProjectController.cs
@@ -0,0 +1,98 @@
+﻿using Kendo.Mvc.UI;
+using PCDCRSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+
+namespace PCDCRSystem.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // GET: Project
+        public ActionResult ProjectManagement()
+        {
+            return View();
+        }
+
+        public JsonResult AllProjects([DataSourceRequest] DataSourceRequest request)
+        {
+            using (PCDCREntities db = new PCDCREntities())
+            {
+                var q = db.Projects_table.ToList();
+                return Json(q.ToDataSourceResult(request));
+            }
+        }
+
+
+        // Insert New
+        public JsonResult Create_Project([DataSourceRequest] DataSourceRequest request, Projects_table project)
+        {
+            using (PCDCREntities db = new PCDCREntities())
+            {
+                if (project != null && ModelState.IsValid)
+                {
+                    Projects_table p = new Projects_table();
+
+
+
+                    p.ProjectName = project.ProjectName;
+                    p.StartDate = project.StartDate;
+                    p.EndDate = project.EndDate;
+                    p.ProjectStatus = project.ProjectStatus;
+
+                    p.ProgrameID = project.ProgrameID;
+
+
+
+
+                    db.Projects_table.Add(p);
+                    db.SaveChanges();
+                    project.ID = p.ID;
+                }
+
+                return Json(new[] { project }.ToDataSourceResult(request, ModelState));
+            }
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public JsonResult Update_Project([DataSourceRequest] DataSourceRequest request, Projects_table project)
+        {
+            using (PCDCREntities db = new PCDCREntities())
+            {
+                if (project != null && ModelState.IsValid)
+                {
+                    Projects_table p = db.Projects_table.Single(c => c.ID == project.ID);
+
+                    p.ProjectName = project.ProjectName;
+                    p.StartDate = project.StartDate;
+                    p.EndDate = project.EndDate;
+                    p.ProjectStatus = project.ProjectStatus;
+
+                    p.ProgrameID = project.ProgrameID;
+
+                }
+                db.SaveChanges();
+                return Json(ModelState.IsValid ? true : ModelState.ToDataSourceResult());
+            }
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public JsonResult Delete_Project([DataSourceRequest] DataSourceRequest request, Projects_table project)
+        {
+            using (PCDCREntities db = new PCDCREntities())
+            {
+                if (project != null && ModelState.IsValid)
+                {
+                    Projects_table p = db.Projects_table.Single(c => c.ID == project.ID);
+                    db.Projects_table.Remove(p);
+                }
+
+                db.SaveChanges();
+                return Json(ModelState.IsValid ? true : ModelState.ToDataSourceResult());
+            }
+        }
+
+    }
+}
---
+++ PCDCRSystem/PCDCRSystem/Controllers/UsersController.cs
@@ -0,0 +1,93 @@
+﻿using Kendo.Mvc.UI;
+using PCDCRSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Kendo.Mvc.Extensions;
+
+namespace PCDCRSystem.Controllers
+{
+    public class UsersController : Controller
+    {
+        // GET: Users
+        public ActionResult UserManegment()
+        {
+            return View();
+        }
+
+        public JsonResult TheUsers([DataSourceRequest] DataSourceRequest request)
+        {
+            using (PCDCREntities db = new PCDCREntities())
+            {
+                var q = db.Users_Table.ToList();
+                return Json(q.ToDataSourceResult(request));
+            }
+        }
+
+
+        // Insert New
+        public JsonResult Create_User([DataSourceRequest] DataSourceRequest request, Users_Table usr)
+        {
+            using (PCDCREntities db = new PCDCREntities())
+            {
+                if (usr != null && ModelState.IsValid)
+                {
+                    Users_Table u = new Users_Table();
+
+                    u.UserName = usr.UserName;
+                    u.Password = usr.Password;
+                    u.UserAddress = usr.UserAddress;
+                    u.UserPhone = usr.UserPhone;
+                    u.Status = usr.Status;
+                    u.UserType = usr.UserType;
+
+                    db.Users_Table.Add(u);
+                    db.SaveChanges();
+                    usr.ID = u.ID;
+                }
+
+                return Json(new[] { usr }.ToDataSourceResult(request, ModelState));
+            }
+        }
+        [AcceptVerbs(HttpVerbs.Post)]
+        public JsonResult Update_User([DataSourceRequest] DataSourceRequest request, Users_Table usr)
+        {
+            using (PCDCREntities db = new PCDCREntities())
+            {
+                if (usr != null && ModelState.IsValid)
+                {
+                    Users_Table u = db.Users_Table.Single(c => c.ID == usr.ID);
+
+
+                    u.UserName = usr.UserName;
+                    u.Password = usr.Password;
+                    u.UserAddress = usr.UserAddress;
+                    u.UserPhone = usr.UserPhone;
+                    u.Status = usr.Status;
+                    u.UserType = usr.UserType;
+
+                }
+                db.SaveChanges();
+                return Json(ModelState.IsValid ? true : ModelState.ToDataSourceResult());
+            }
+        }
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public JsonResult Delete_User([DataSourceRequest] DataSourceRequest request, Users_Table usr)
+        {
+            using (PCDCREntities db = new PCDCREntities())
+            {
+                if (usr != null && ModelState.IsValid)
+                {
+                    Users_Table u = db.Users_Table.Single(c => c.ID == usr.ID);
+                    db.Users_Table.Remove(u);
+                }
+
+                db.SaveChanges();
+                return Json(ModelState.IsValid ? true : ModelState.ToDataSourceResult());
+            }
+        }
+    }
+}
