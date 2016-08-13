af2f056866e29e85015e62eced3c38257accde1d
ainma007 <b-sword@outlook.com>
2016-08-13 20:36:56 +0300

    ترتيب السيشن وصفحة الدخول مبدئي

--- PCDCRSystem/PCDCRSystem/Controllers/AccountController.cs
+++ PCDCRSystem/PCDCRSystem/Controllers/AccountController.cs
@@ -41,36 +41,9 @@ namespace PCDCRSystem.Controllers
                             Session["Password"] = u.Password;
 
 
-                            // Check If Already LogIned Or Not : 
-                            //bool CheckCurrentUser = IsOnLine(u.ID);
-                            //if (CheckCurrentUser == false)
-                            //{
-                            //    // اذا كان المستخدم 
-                            //    // OfLine 
-                            //    //  يدخل على النظام
-                            //    // ونسجل بياناته  في جدول 
-                       
-                            //    //LogHistory h = new LogHistory()
-                            //    //{
-                            //    //    UserId = u.ID,
-                            //    //    LogInTime = DateTime.Now,
-                            //    //    Status = "OnLine",
-                            //    //    IPAddress = GetLocalIPAddress()
-
-                            //    //};
-                            //    //dc.LogHistory.Add(h);
-                            //    //dc.SaveChanges();
+                         
                               return RedirectToAction("LoggedIn");
-                            //}
-                            //else
-                            //{
-                            //    //  اذا كان مسجل دخول  يعني OnLine
-                            //    //
-
-                            //    return RedirectToAction("LoggedIn");
-                            //    // return View()ك
-                            //}
-
+                           
 
                         }
                     }
